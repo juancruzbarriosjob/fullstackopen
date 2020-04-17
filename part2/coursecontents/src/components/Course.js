a9a686385ad2acb9a65df50f3151926326f320e1
Juan Cruz Barrios <juancruzbarrios.job@gmail.com>
2020-04-17 18:00:28 +0200

    Part 2 - course content from 2.1 to 2.5 update

--- part2/coursecontents/src/components/Course.js
+++ part2/coursecontents/src/components/Course.js
@@ -1,38 +1,16 @@
 import React from 'react'
+import Part from './Part'
+import Total from './Total'
 
 const Course = ({ course }) => {
   return (
     <>
-    <SubHeader name={course.name} />
+    <h2>{course.name}</h2>
     {course.parts.map((part) =>
       <Part key={part.id} part={part.name} exercise={part.exercises} />
     )}
     <Total parts={course.parts} />
     </>
-  )
-}
-
-const SubHeader = ({ name }) => {
-  return (
-    <h2>{name}</h2>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercise}
-    </p>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((carry, part) => {
-    return carry += part.exercises
-  }, 0)
-
-  return (
-    <p><b>Total of {total} exercises</b></p>
   )
 }
 
