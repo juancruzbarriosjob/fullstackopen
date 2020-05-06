a82e08b1bd3dfd304b35bf39e35ee1b134b53b8b
Juan Cruz Barrios <juancruzbarrios.job@gmail.com>
2020-05-06 16:15:02 +0200

    Part 4 - blog-list 4.1 - 4.2 refactor

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -4,7 +4,6 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -18,10 +17,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-//app.use(middleware.)
 
 app.use('/api/blogs', blogsRouter)
-
-//app.use(middleware.)
 
 module.exports = app
